6f8aa573868b825e24733a3ff660e4f8d544e0ad
unknown <mgehring9@gmail.com>
2020-04-02 11:55:55 -0700

    add brick collision detection

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,9 +6,12 @@ public class Ball : MonoBehaviour
 {
     //config parameters
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
 
     //State
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +21,25 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (hasStarted == false)
+        {
+            LockBallToPaddle();
+            Launch();
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void Launch()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+        }
     }
 }
